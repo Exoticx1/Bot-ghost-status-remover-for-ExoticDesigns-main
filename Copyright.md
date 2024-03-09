b4521cb07d7313b8f25c25521f9514ec4ac48ec1
Exoticx1 <162768099+Exoticx1@users.noreply.github.com>
2024-03-08 21:00:31 -0500

    Add files via upload

---
+++ Copyright.md
@@ -0,0 +1,22 @@
+## ©Exotic Designs @2023
+All rights reserved.
+
+The code, and content of this application are protected by international copyright laws. Unauthorized reproduction or distribution of this application or any portion of it may result in severe civil and criminal penalties and will be prosecuted to the maximum extent possible under the law.
+
+Exotic Designs retains all rights, including but not limited to the right to use, copy, distribute, and make derivative works of this application.
+
+For inquiries about licensing, please contact [exoticdesigns] via discord.
+
+/**
+
+███████╗██╗░░██╗░█████╗░████████╗██╗░█████╗░  ██████╗░███████╗░██████╗██╗░██████╗░███╗░░██╗░██████╗
+██╔════╝╚██╗██╔╝██╔══██╗╚══██╔══╝██║██╔══██╗  ██╔══██╗██╔════╝██╔════╝██║██╔════╝░████╗░██║██╔════╝
+█████╗░░░╚███╔╝░██║░░██║░░░██║░░░██║██║░░╚═╝  ██║░░██║█████╗░░╚█████╗░██║██║░░██╗░██╔██╗██║╚█████╗░
+██╔══╝░░░██╔██╗░██║░░██║░░░██║░░░██║██║░░██╗  ██║░░██║██╔══╝░░░╚═══██╗██║██║░░╚██╗██║╚████║░╚═══██╗
+███████╗██╔╝╚██╗╚█████╔╝░░░██║░░░██║╚█████╔╝  ██████╔╝███████╗██████╔╝██║╚██████╔╝██║░╚███║██████╔╝
+╚══════╝╚═╝░░╚═╝░╚════╝░░░░╚═╝░░░╚═╝░╚════╝░  ╚═════╝░╚══════╝╚═════╝░╚═╝░╚═════╝░╚═╝░░╚══╝╚═════╝░       
+
+ * **********************************************
+ *   Code by Exotic Designs
+ * **********************************************
+ */
---
+++ index.js
@@ -0,0 +1,110 @@
+/**
+███████╗██╗░░██╗░█████╗░████████╗██╗░█████╗░  ██████╗░███████╗░██████╗██╗░██████╗░███╗░░██╗░██████╗
+██╔════╝╚██╗██╔╝██╔══██╗╚══██╔══╝██║██╔══██╗  ██╔══██╗██╔════╝██╔════╝██║██╔════╝░████╗░██║██╔════╝
+█████╗░░░╚███╔╝░██║░░██║░░░██║░░░██║██║░░╚═╝  ██║░░██║█████╗░░╚█████╗░██║██║░░██╗░██╔██╗██║╚█████╗░
+██╔══╝░░░██╔██╗░██║░░██║░░░██║░░░██║██║░░██╗  ██║░░██║██╔══╝░░░╚═══██╗██║██║░░╚██╗██║╚████║░╚═══██╗
+███████╗██╔╝╚██╗╚█████╔╝░░░██║░░░██║╚█████╔╝  ██████╔╝███████╗██████╔╝██║╚██████╔╝██║░╚███║██████╔╝
+╚══════╝╚═╝░░╚═╝░╚════╝░░░░╚═╝░░░╚═╝░╚════╝░  ╚═════╝░╚══════╝╚═════╝░╚═╝░╚═════╝░╚═╝░░╚══╝╚═════╝░        
+ * **********************************************
+ *   Code by EXOTIC DESIGNS
+ * **********************************************
+ */
+
+
+
+const { Client, GatewayIntentBits, ActivityType, TextChannel } = require('discord.js');
+require('dotenv').config();
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const client = new Client({
+  intents: Object.keys(GatewayIntentBits).map((a) => {
+    return GatewayIntentBits[a];
+  }),
+});
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  res.send('YaY Your Bot Status Changed✨');
+});
+app.listen(port, () => {
+  console.log(`🔗 Listening to RTX: http://localhost:${port}`);
+  console.log(`🔗 Powered By RTX`);
+});
+
+
+const statusMessages = [" "," "];
+
+
+let currentIndex = 0;
+const channelId = '';
+
+async function login() {
+  try {
+    await client.login(process.env.TOKEN);
+    console.log(`\x1b[36m%s\x1b[0m`, `|    🐇 Logged in as ${client.user.tag}`);
+  } catch (error) {
+    console.error('Failed to log in:', error);
+    process.exit(1);
+  }
+}
+
+/**
+███████╗██╗░░██╗░█████╗░████████╗██╗░█████╗░  ██████╗░███████╗░██████╗██╗░██████╗░███╗░░██╗░██████╗
+██╔════╝╚██╗██╔╝██╔══██╗╚══██╔══╝██║██╔══██╗  ██╔══██╗██╔════╝██╔════╝██║██╔════╝░████╗░██║██╔════╝
+█████╗░░░╚███╔╝░██║░░██║░░░██║░░░██║██║░░╚═╝  ██║░░██║█████╗░░╚█████╗░██║██║░░██╗░██╔██╗██║╚█████╗░
+██╔══╝░░░██╔██╗░██║░░██║░░░██║░░░██║██║░░██╗  ██║░░██║██╔══╝░░░╚═══██╗██║██║░░╚██╗██║╚████║░╚═══██╗
+███████╗██╔╝╚██╗╚█████╔╝░░░██║░░░██║╚█████╔╝  ██████╔╝███████╗██████╔╝██║╚██████╔╝██║░╚███║██████╔╝
+╚══════╝╚═╝░░╚═╝░╚════╝░░░░╚═╝░░░╚═╝░╚════╝░  ╚═════╝░╚══════╝╚═════╝░╚═╝░╚═════╝░╚═╝░░╚══╝╚═════╝░        
+ * **********************************************
+ *   Code by EXOTIC DESIGNS
+ * **********************************************
+ */
+
+
+function updateStatusAndSendMessages() {
+  const currentStatus = statusMessages[currentIndex];
+  const nextStatus = statusMessages[(currentIndex + 1) % statusMessages.length];
+
+  client.user.setPresence({
+    activities: [{ name: currentStatus, type: ActivityType.Custom}],
+    status: 'dnd',
+  });
+
+  
+  const textChannel = client.channels.cache.get(channelId);
+
+  if (textChannel instanceof TextChannel) {
+   
+    textChannel.send(`Bot status is: ${currentStatus}`);
+  } else {
+
+  }
+
+  currentIndex = (currentIndex + 1) % statusMessages.length;
+}
+
+client.once('ready', () => {
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ✅ Bot is ready as ${client.user.tag}`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ✨HAPPY NEW YEAR MY DEAR FAMILY`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ❤️WELCOME TO 2024`);
+  updateStatusAndSendMessages();
+
+  setInterval(() => {
+    updateStatusAndSendMessages();
+  }, 10000);
+});
+
+login();
+
+/**
+███████╗██╗░░██╗░█████╗░████████╗██╗░█████╗░  ██████╗░███████╗░██████╗██╗░██████╗░███╗░░██╗░██████╗
+██╔════╝╚██╗██╔╝██╔══██╗╚══██╔══╝██║██╔══██╗  ██╔══██╗██╔════╝██╔════╝██║██╔════╝░████╗░██║██╔════╝
+█████╗░░░╚███╔╝░██║░░██║░░░██║░░░██║██║░░╚═╝  ██║░░██║█████╗░░╚█████╗░██║██║░░██╗░██╔██╗██║╚█████╗░
+██╔══╝░░░██╔██╗░██║░░██║░░░██║░░░██║██║░░██╗  ██║░░██║██╔══╝░░░╚═══██╗██║██║░░╚██╗██║╚████║░╚═══██╗
+███████╗██╔╝╚██╗╚█████╔╝░░░██║░░░██║╚█████╔╝  ██████╔╝███████╗██████╔╝██║╚██████╔╝██║░╚███║██████╔╝
+╚══════╝╚═╝░░╚═╝░╚════╝░░░░╚═╝░░░╚═╝░╚════╝░  ╚═════╝░╚══════╝╚═════╝░╚═╝░╚═════╝░╚═╝░░╚══╝╚═════╝░        
+ * **********************************************
+ *   Code by EXOTIC DESIGNS
+ * **********************************************
+ */
